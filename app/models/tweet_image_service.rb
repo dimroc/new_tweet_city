fb0ca185c4fe41b31ee8c761952e10386589b672
Dimitri Roche <dimroc@gmail.com>
2013-07-08 19:40:38 -0400

    Color pixels based on age and coordinate frequency

--- app/models/tweet_image_service.rb
+++ app/models/tweet_image_service.rb
@@ -8,6 +8,9 @@ class TweetImageService
 
     @mercator_width = Tweet::NYC_TOP_RIGHT.x - Tweet::NYC_BOTTOM_LEFT.x
     @mercator_height = Tweet::NYC_TOP_RIGHT.y - Tweet::NYC_BOTTOM_LEFT.y
+
+    @earliest = Tweet.order(:created_at).first.created_at
+    @latest = Tweet.order("created_at DESC").first.created_at
   end
 
   def save(filename)
@@ -16,15 +19,27 @@ class TweetImageService
     end
 
     @png.save filename
+    FileUtils.cp filename, "tmp/latest.png"
   end
 
   private
 
   def set_pixel(tweet)
-    @png.set_pixel(
+    pixel_coordinates = [
       map_x(tweet.point),
-      map_y(tweet.point),
-      ChunkyPNG::Color.rgba(0, 0, 0, 255))
+      map_y(tweet.point)]
+
+    color = ChunkyPNG::Color.rgba(
+      color_age(tweet.created_at),
+      0,
+      color_frequency(pixel_coordinates),
+      255)
+
+    @png.set_pixel(
+      pixel_coordinates[0],
+      pixel_coordinates[1],
+      color)
+
   rescue ChunkyPNG::OutOfBounds
     puts "skipping pixel, out of bounds"
   end
@@ -44,5 +59,23 @@ class TweetImageService
 
     # ChunkyPNG has row 0 at the top, not bottom of image. Flip
     @width - mapped
+  end
+
+  def color_age(date)
+    offset = date.to_i - @earliest.to_i
+    range = @latest.to_i - @earliest.to_i
+    ((offset.to_f / range) * 255).to_i
+  end
+
+  def color_frequency(pixel_coordinates)
+    @frequency_buckets ||= {}
+    key = "#{pixel_coordinates[0]},#{pixel_coordinates[1]}".to_sym
+    if @frequency_buckets[key]
+      @frequency_buckets[key] += 1
+    else
+      @frequency_buckets[key] = 1
+    end
+
+    ((@frequency_buckets[key].to_f / 25) * 255).to_i
   end
 end
