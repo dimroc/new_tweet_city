c752ba0a97817621c722f67ded6d97dfa9b48f7c
Dimitri Roche <dimroc@gmail.com>
2013-07-20 15:01:45 -0400

    Add precompilation paths to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ NewTweetCity::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.woff *.ttf *.eot *.svg)
 
   # don't initialize the app when 'rake assets:precompile'
   config.assets.initialize_on_precompile = false
