def06e3b1a27148a1bf5507a4e0b6e0c0ae2e0d0
Dimitri Roche <dimroc@gmail.com>
2013-07-27 20:11:28 -0400

    snapshot index page has bg of latest snapshot

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def controller_and_action
+    controller_name = params[:controller].gsub('/', ' ')
+    "#{controller_name} #{params[:action]}"
+  end
+
+  def last_snapshot_image(area)
+    Snapshot.public_send(area).ascending.last.url
+  end
 end
