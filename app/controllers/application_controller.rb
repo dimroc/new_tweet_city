fc7851e55225329e840084285c18e1bf3df614ac
Dimitri Roche <dimroc@gmail.com>
2013-08-07 08:35:54 -0400

    Set character set for all pages to utf-8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_charset
+
+  private
+
+  def set_charset
+    response.headers['Content-type'] = 'text/html; charset=utf-8'
+  end
 end
