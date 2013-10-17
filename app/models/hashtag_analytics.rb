5311d647957dcd45fc876047973e75211fd35597
Dimitri Roche <dimroc@gmail.com>
2013-10-17 01:23:00 -0400

    HashtagAnalytics also analyzes all of NYC

--- app/models/hashtag_analytics.rb
+++ app/models/hashtag_analytics.rb
@@ -23,9 +23,31 @@ class HashtagAnalytics < ActiveRecord::Base
     analytics
   end
 
+  def self.generate_for_nyc(period, entry_count = 5)
+    start_date = 1.send(period).ago
+    analytics = HashtagAnalytics.new(period: period)
+
+    Hashtag.select("term, COUNT(id)").
+      where("created_at > ?", start_date).
+      group("term").
+      order("COUNT(id) DESC").first(entry_count).each do |entry|
+        print '.'
+        analytics.entries.build(
+          term: entry.term,
+          count: entry.count)
+      end
+
+    analytics.save!
+    analytics
+  end
+
   def self.generate_for_boroughs(period, entry_count = 5)
     Borough.names.each do |borough|
       generate(borough, period, entry_count)
     end
+  end
+
+  def all_nyc?
+    borough.blank? && neighborhood.blank?
   end
 end
--- lib/tasks/hashtag.rake
+++ lib/tasks/hashtag.rake
@@ -2,6 +2,7 @@ namespace :hashtag do
   desc "Analyze all tweets and generate hashtags"
   task :analyze_hour => :environment do
     HashtagAnalytics.generate_for_boroughs('hour')
+    HashtagAnalytics.generate_for_nyc('hour')
   end
 
   desc "Nightly analysis task"
@@ -9,9 +10,11 @@ namespace :hashtag do
 
   task :analyze_day => :environment do
     HashtagAnalytics.generate_for_boroughs('day')
+    HashtagAnalytics.generate_for_nyc('day')
   end
 
   task :analyze_week => :environment do
     HashtagAnalytics.generate_for_boroughs('week', 10)
+    HashtagAnalytics.generate_for_nyc('week', 10)
   end
 end
