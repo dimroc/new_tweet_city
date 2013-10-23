01396bcd214d5e35f5228778b82faef76976455a
Dimitri Roche <dimroc@gmail.com>
2013-10-22 23:46:20 -0400

    Allow users to show hashtag analytics by day

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,16 +1,18 @@
 class HashtagsController < ApplicationController
   def index
-    @hours = analytics_for('hour')
-    @days = analytics_for('day')
-    @weeks = analytics_for('week')
+    @date = params[:date].try(:to_date) || DateTime.now
+    @hours = analytics_for('hour', @date)
+    @days = analytics_for('day', @date)
+    @weeks = analytics_for('week', @date)
   end
 
   private
 
-  def analytics_for(period)
+  def analytics_for(period, date = DateTime.now)
     analytics = {}
     Borough.names.each do |borough|
       analytics[borough] = HashtagAnalytics.
+        where(created_at: (date - 1.day)..date).
         borough(borough).
         period(period).
         last
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,7 @@ module ApplicationHelper
   end
 
   def analytics(hashtag_analytics, number_of_entries = 3)
+    return unless hashtag_analytics
     rval = []
     hashtag_analytics.entries.first(number_of_entries).each do |entry|
       link = link_to("##{entry.term}", "https://twitter.com/search?src=typd&q=%23#{entry.term}")
@@ -16,5 +17,12 @@ module ApplicationHelper
     end
 
     content_tag(:ol, rval.join.html_safe)
+  end
+
+  def analytics_hour(hours)
+    hours.each do |key, value|
+      return value.created_at.strftime("%I%p") if value
+    end
+    nil
   end
 end
