41ee9aa7010e181266f760f65dcd1f9d04082a01
Dimitri Roche <dimroc@gmail.com>
2014-01-25 10:36:01 -0500

    Remove hateful comments

--- lib/mercator.rb
+++ lib/mercator.rb
@@ -2,7 +2,7 @@
 class Mercator
   SRID = 3785
 
-  # This PROJ4 string is actually INCORRECT (thanks dazuma) but
+  # This PROJ4 string is actually INCORRECT but
   # because of legacy with my own apps, decided to continue with same projection
   FACTORY = RGeo::Geographic.projected_factory(
         projection_proj4: '+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs',
