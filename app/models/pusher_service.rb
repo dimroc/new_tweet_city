11e834e4a0ebdc9395bd47b3856ab0087e3a0aec
Dimitri Roche <dimroc@gmail.com>
2014-04-15 20:53:19 -0400

    Create TweetView class for PusherService.broadcast

--- app/models/pusher_service.rb
+++ app/models/pusher_service.rb
@@ -8,32 +8,33 @@ class PusherService
         channel,
         body: output,
         neighborhood: tweet.neighborhood.slug,
-        media_type: tweet.media_type
-      )
+        media_type: tweet.media_type)
     rescue => e
       puts "Error pushing tweet: #{e.message}"
     end
 
     def render_tweet(tweet)
-      view = ActionView::Base.new(ActionController::Base.view_paths)
-      view.class_eval do
-        include Rails.application.routes.url_helpers
-        include ApplicationHelper
-
-        def protect_against_forgery?
-          false
-        end
-
-        def default_url_options
-          if Rails.env.production?
-            {host: "newtweetcity.com"}
-          else
-            {host: 'localhost:3000'}
-          end
-        end
-      end
-
+      view = TweetView.new(ActionController::Base.view_paths)
       view.render(partial: 'boroughs/tweet', locals: { tweet: tweet })
+    end
+  end
+
+  private
+
+  class TweetView < ActionView::Base
+    include Rails.application.routes.url_helpers
+    include ApplicationHelper
+
+    def protect_against_forgery?
+      false
+    end
+
+    def default_url_options
+      if Rails.env.production?
+        {host: "newtweetcity.com"}
+      else
+        {host: 'localhost:3000'}
+      end
     end
   end
 end
