1bf2f3c6f0f9f0ef413825d4ebd3b7f2bc07f8aa
Dimitri Roche <dimroc@gmail.com>
2014-07-18 01:10:09 -0400

    Tweet index holds neighborhood values as not_analyzed
    TODO: Still issues working with ES with not_analyzed

--- app/models/concerns/searchable_tweet.rb
+++ app/models/concerns/searchable_tweet.rb
@@ -5,17 +5,15 @@ module SearchableTweet
     include Elasticsearch::Model
     index_name    "tweets-#{Rails.env}"
 
-    settings do
+    settings index: { number_of_shards: 1, number_of_replicas: 0 } do
       mappings dynamic: false do
         indexes :screen_name
         indexes :text, analyzer: 'english'
         indexes :created_at, type: 'date'
 
-        indexes :neighborhood do
-          indexes :name
-          indexes :slug
-          indexes :borough
-        end
+        indexes :neighborhood, index: 'not_analyzed'
+        indexes :neighborhood_slug, index: 'not_analyzed'
+        indexes :borough, index: 'not_analyzed'
       end
     end
 
@@ -25,18 +23,24 @@ module SearchableTweet
 
     # Customize the JSON serialization for Elasticsearch
     def as_indexed_json(options={})
-      self.as_json(
-        only: [:screen_name, :text, :created_at],
-        include: {
-          neighborhood: { only: [:name, :slug, :borough]}
-        })
-    end
+      rval = self.as_json(only: [:screen_name, :text, :created_at])
+      neighborhood_json = {
+        neighborhood: neighborhood.name,
+        neighborhood_slug: neighborhood.slug,
+        borough: neighborhood.borough
+      }
 
-    def self.fast_import
+      rval.merge neighborhood_json
+    end
+  end
+
+  module ClassMethods
+    def fast_import(options={}, &block)
       neighborhood_cache = Neighborhood.generate_cache
       transform = lambda do |t|
         response = {
           _id: t.id,
+          _score: 1,
           screen_name: t.screen_name,
           text: t.text,
           created_at: t.created_at,
@@ -54,7 +58,29 @@ module SearchableTweet
         { index: response }
       end
 
-      import transform: transform
+      import(options.merge({transform: transform}), &block)
+    end
+
+    def search_count_in_hoods(definition)
+      results = Tweet.search(
+        { query: { match: { text: 'knicks' } }, aggregations: { neighborhoods: { terms: { field: 'neighborhood_slug' } } } },
+        search_type: 'count')
+
+      results.response.aggregations.neighborhoods.buckets.inject({}) do |memo, term|
+        memo[term['key']] = term['doc_count']
+        memo
+      end
+    end
+
+    def search_count_in_boroughs(definition)
+      results = Tweet.search(
+        { query: { match: { text: 'knicks' } }, aggregations: { boroughs: { terms: { field: 'borough' } } } },
+        search_type: 'count')
+
+      results.response.aggregations.boroughs.buckets.inject({}) do |memo, term|
+        memo[term['key']] = term['doc_count']
+        memo
+      end
     end
   end
 end
